a20a26adffe9104fdf72cbc95a89a206cb1fbae5
JuanPe1001 <juanpemuferre@gmail.com>
2023-11-16 15:04:50 -0300

    hola

---
+++ bot.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import commands
+import os,random , requests
+intents = discord.Intents.default()
+intents.message_content = True
+def get_zorro_image_url():    
+    url = ' https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['link']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['link']
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    img_name=random.choice(os.listdir('memes'))
+    with open(f'memes/{img_name}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('zorro')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('dog')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("MTE2MzU5MTQxMjczNTAyMTI2Nw.GDVw2I.wmjHDNHgpJ4kRW0r18p_U0x7IuTkN8vItEQApY")
